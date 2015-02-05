8865487a8a07bff05449b7c3a238e889e93bf8a5
Z.Li <lizhi.magic@gmail.com>
2015-02-05 17:57:42 -0500

    first

---
+++ auto_car_slam/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/zhili/hector_slam_tutorial/auto_car_slam/src
+# Build directory: /home/zhili/hector_slam_tutorial/auto_car_slam/build
+# 
+# This file includes the relevent testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+SUBDIRS(gtest)
+SUBDIRS(car_slam)
